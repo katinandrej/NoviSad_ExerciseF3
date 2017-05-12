a10cd127fe47e37668862b760a2fd16395b43c3e
somename <someemail>
2017-05-12 20:27:43 +0200

    besouro automatic message

---
+++ src/Explorer.java
@@ -0,0 +1,54 @@
+
+public class Explorer {
+	private int x;
+	private int y;
+	private String direction;
+	private String[] posibleDirRight = {"N", "E", "S", "W"};
+	private String[] posibleDirLeft = {"N", "W", "S", "E"};
+	
+	public Explorer () {
+		
+	}
+	
+	public Explorer (int x, int y, String direction) {
+		this.x = x;
+		this.y = y;
+		this.direction = direction;
+	}
+	
+	public void movingRight (String dir) {
+		for (int i = 0; i < posibleDirRight.length; i++) {
+			if (dir == posibleDirRight[i]) {
+				dir = posibleDirRight[i]+1;
+			}
+		}
+	}
+	
+	public String toString() {
+		return this.direction;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getDirection() {
+		return direction;
+	}
+
+	public void setDirection(String direction) {
+		this.direction = direction;
+	}
+}

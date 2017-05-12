4b973343c5e3208bb3c27002f0767cb2f2faf02f
somename <someemail>
2017-05-12 20:28:20 +0200

    besouro automatic message

--- src/PlanetExplorer.java
+++ src/PlanetExplorer.java
@@ -4,6 +4,12 @@
 // Student ID:
 
 public class PlanetExplorer {
+	
+	private int x;
+	private int y;
+	private String obstacles;
+	private Explorer one;
+	
 	public PlanetExplorer(int x, int y, String obstacles){
 	/*	x and y represent the size of the grid.
 	 *  Obstacles is a String formatted as follows: "(obs1_x,obs1_y)(obs2_x,obs2_y)...(obsN_x,obsN_y)" with no white spaces. 
@@ -12,7 +18,18 @@ public class PlanetExplorer {
 		PlanetExplorer explorer = new PlanetExplorer(100,100,"(5,5)(7,8)")  
 		 
 	 */
+		
+		this.x = x;
+		this.y = y;
+		this.obstacles = obstacles;
 	}
+	
+	public PlanetExplorer(int x, int y, String obstacles, Explorer one) {
+			this.x = x;
+			this.y = y;
+			this.obstacles = obstacles;
+			this.one = one;
+		}
 	
 	public String executeCommand(String command){
 		
@@ -27,5 +44,30 @@ public class PlanetExplorer {
 		 */
 		
 		return null;
+	}
+	
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public String getObstacles() {
+		return obstacles;
+	}
+
+	public void setObstacles(String obstacles) {
+		this.obstacles = obstacles;
 	}
 }
